b1f04726fb50f88a05db97058c8cf6a6b0bee146
Dauboau <86164187+Dauboau@users.noreply.github.com>
2023-11-28 09:32:06 -0300

    Transmissor
    Java porque Java é bom. kkkkkk

---
+++ simulaEnlace/src/App.java
@@ -0,0 +1,16 @@
+import java.math.BigInteger;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+import java.util.stream.Collectors;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        
+        // chama a próxima camada
+        AppTransmissor.aplicacaoTransmissora();
+
+    }
+    
+}
